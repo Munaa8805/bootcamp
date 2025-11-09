abd4e4c18dc1419477465fa7655c4dff03415b2f
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-11-08 23:00:43 -0700

    created route file

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
 
+const bootcamps = require("./routes/bootcamps");
 
 //// Load env vars
-// dotenv.config({ path: './config/config.env' });
+dotenv.config({
+  path: "./config/config.env",
+  //   override: true,
+  //   debug: true,
+  //   processEnv: true,
+});
 
 const app = express();
+app.use("/api/v1/bootcamps", bootcamps);
 
-const PORT =  4000;
 
-app.get('/', (req, res) => {
-  res.send('Bootcamper API is running...');
-});
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in mode on port ${PORT}`);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
-
