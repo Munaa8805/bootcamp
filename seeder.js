9ae5bcb107e6e74427fd51f5cc791f323f0c4812
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-11-11 22:45:05 -0700

    feat: Create seeder file and json data

---
+++ seeder.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Bootcamp = require("./models/Bootcamp");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("MongoDB Connected...");
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/bootcamps.json`, "utf-8")
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log("Data Imported...");
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log("Data Destroyed...");
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
